8546d2ed9d38e486de65917fa82cf060d96f593d
part1kl <jsydie@gmail.com>
2018-08-16 01:53:50 -0400

    Update

---
+++ HomeKit-Deployment/src/net/part1kl/homekit/deployment/executables/Testing.java
@@ -0,0 +1,51 @@
+/**
+ * See README for licensing information
+*/
+package net.part1kl.homekit.deployment.executables;
+
+/** This class is for Deployment testing only
+ * 
+ * 
+ * <br><br>
+ * <style>
+ *		table {
+ *  		border-collapse: collapse;
+ *		}
+ *		table, tr {
+ *			border: 1px solid black;
+ *		}
+ *	</style>
+ * <table>
+ * 		<tr>
+ * 			<th> Project: </th>
+ * 			<td> HomeKit-Deployment </td>
+ * 		</tr>
+ * 		<tr>
+ * 			<th> Package: </th>
+ * 			<td> net.part1kl.homekit.deployment.executables </td>
+ * 		</tr>
+ * 		<tr>
+ * 			<th> Class: </th>
+ * 			<td> Testing </td>
+ * 		</tr>
+ * 		<tr>
+ * 			<th> Version: </th>
+ * 			<td> 0.1 </td>
+ * 		</tr>
+ * 		<tr>
+ * 			<th> Date Created: </th>
+ * 			<td> Aug 16, 2018 </td>
+ * 		</tr>
+ * 		<tr>
+ * 			<th> Author: </th>
+ * 			<td> part1kl </td>
+ * 		</tr>
+ * 		<tr>
+ * 			<th> Year: </th>
+ * 			<td> 2018 </td>
+ * 		</tr>
+ * </table>
+ */
+public class Testing {
+
+}
