02ef16553eda970139e874cad6abbd5e545a164a
part1kl <jsydie@gmail.com>
2018-08-16 01:47:44 -0400

    Initial commit of Core Project

---
+++ HomeKit-Core/src/net/part1kl/homekit/core/comm/package-info.java
@@ -0,0 +1,47 @@
+/**
+ * See README for licensing information
+*/
+/** This package contains basic communication classes for all HomeKit applications.
+ * 
+ * 
+ * <br><br>
+ * <style>
+ *		table {
+ *  		border-collapse: collapse;
+ *		}
+ *		table, tr {
+ *			border: 1px solid black;
+ *		}
+ *	</style>
+ * <table>
+ * 		<tr>
+ * 			<th> Project: </th>
+ * 			<td> HomeKit-Core </td>
+ * 		</tr>
+ * 		<tr>
+ * 			<th> Package: </th>
+ * 			<td> net.part1kl.homekit.core.comm </td>
+ * 		</tr>
+ * 		<tr>
+ * 			<th> Class: </th>
+ * 			<td> package-info </td>
+ * 		</tr>
+ * 		<tr>
+ * 			<th> Version: </th>
+ * 			<td> 0.1 </td>
+ * 		</tr>
+ * 		<tr>
+ * 			<th> Date Created: </th>
+ * 			<td> Aug 16, 2018 </td>
+ * 		</tr>
+ * 		<tr>
+ * 			<th> Author: </th>
+ * 			<td> part1kl </td>
+ * 		</tr>
+ * 		<tr>
+ * 			<th> Year: </th>
+ * 			<td> 2018 </td>
+ * 		</tr>
+ * </table>
+ */
+package net.part1kl.homekit.core.comm;
---
+++ HomeKit-Deployment/src/net/part1kl/homekit/deployment/executables/package-info.java
@@ -0,0 +1,47 @@
+/**
+ * See README for licensing information
+*/
+/** This package contains executable classes for HomeKit Deployment applications
+ * 
+ * 
+ * <br><br>
+ * <style>
+ *		table {
+ *  		border-collapse: collapse;
+ *		}
+ *		table, tr {
+ *			border: 1px solid black;
+ *		}
+ *	</style>
+ * <table>
+ * 		<tr>
+ * 			<th> Project: </th>
+ * 			<td> HomeKit-Deployment </td>
+ * 		</tr>
+ * 		<tr>
+ * 			<th> Package: </th>
+ * 			<td> net.part1kl.homekit.deployment.executables </td>
+ * 		</tr>
+ * 		<tr>
+ * 			<th> Class: </th>
+ * 			<td> package-info </td>
+ * 		</tr>
+ * 		<tr>
+ * 			<th> Version: </th>
+ * 			<td> 0.1 </td>
+ * 		</tr>
+ * 		<tr>
+ * 			<th> Date Created: </th>
+ * 			<td> Aug 16, 2018 </td>
+ * 		</tr>
+ * 		<tr>
+ * 			<th> Author: </th>
+ * 			<td> part1kl </td>
+ * 		</tr>
+ * 		<tr>
+ * 			<th> Year: </th>
+ * 			<td> 2018 </td>
+ * 		</tr>
+ * </table>
+ */
+package net.part1kl.homekit.deployment.executables;
