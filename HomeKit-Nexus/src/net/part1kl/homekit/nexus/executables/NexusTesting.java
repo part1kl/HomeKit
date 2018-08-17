ee1385b04d182e6da7016fb85391cd8d8137547d
part1kl <part1kl@part1kl.net>
2018-08-16 22:33:11 -0400

    Removed Testing and added NexusTesting to differentiate between Nexus and Deployment testing classes in Eclipse

---
+++ HomeKit-Nexus/src/net/part1kl/homekit/nexus/executables/NexusTesting.java
@@ -0,0 +1,87 @@
+/**
+ * See README for licensing information
+*/
+package net.part1kl.homekit.nexus.executables;
+
+import java.io.PrintStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
+
+/** This class is for Nexus testing only.
+ * 
+ * 
+ * <br><br>
+ * <style>
+ *		table {
+ *  		border-collapse: collapse;
+ *		}
+ *		table, tr {
+ *			border: 1px solid black;
+ *		}
+ *	</style>
+ * <table>
+ * 		<tr>
+ * 			<th> Project: </th>
+ * 			<td> HomeKit-Nexus </td>
+ * 		</tr>
+ * 		<tr>
+ * 			<th> Package: </th>
+ * 			<td> net.part1kl.homekit.nexus.executables </td>
+ * 		</tr>
+ * 		<tr>
+ * 			<th> Class: </th>
+ * 			<td> NexusTesting </td>
+ * 		</tr>
+ * 		<tr>
+ * 			<th> Version: </th>
+ * 			<td> 0.1 </td>
+ * 		</tr>
+ * 		<tr>
+ * 			<th> Date Created: </th>
+ * 			<td> Aug 16, 2018 </td>
+ * 		</tr>
+ * 		<tr>
+ * 			<th> Author: </th>
+ * 			<td> part1kl </td>
+ * 		</tr>
+ * 		<tr>
+ * 			<th> Year: </th>
+ * 			<td> 2018 </td>
+ * 		</tr>
+ * </table>
+ */
+public class NexusTesting {
+
+	public static void main(String[] args) throws Exception {
+		Thread clientThread = new Thread(()->{
+			try {
+				Socket client = new Socket("127.0.0.1", 25565);
+				PrintStream clientOut = new PrintStream(client.getOutputStream(), true);
+				Scanner clientIn = new Scanner(client.getInputStream());
+				int clientSendNum = 10;
+				int clientGetNum = 0;
+				clientOut.println(clientSendNum);
+				clientGetNum = clientIn.nextInt();
+				System.out.println(clientGetNum);
+			} catch(Exception e) {e.printStackTrace();}
+		});
+		Thread serverThread = new Thread(()->{
+			try {
+				ServerSocket server = new ServerSocket(25565);
+				Socket connectedClient = server.accept();
+				Scanner clientIn = new Scanner(connectedClient.getInputStream());
+				int received = 0;
+				received = clientIn.nextInt();
+				int sent = received * 2;
+				PrintStream clientOut = new PrintStream(connectedClient.getOutputStream());
+				clientOut.println(sent);
+				
+			}catch(Exception e) {e.printStackTrace();}
+		});
+		
+		serverThread.start();
+		clientThread.start();
+		
+	}
+}
