c6a89b7f74c71aaab3a598e93df8871cc6f33590
part1kl <part1kl@part1kl.net>
2018-08-22 00:24:33 -0400

    I created this little class as a faster way to create ArrayLists. All JobExecution types take a KitArrayList as a way of passing in all the parameters the job will need to perform, and KitArrayLists allow you to add in as many entries as you want when you create the object, so you don't need a whole string of ".add()" lines just to set up parameters for a Job. I will add to this class, no doubt, as I find more ways I can use and improve ArrayLists.

---
+++ HomeKit-Core/src/net/part1kl/homekit/core/util/KitArrayList.java
@@ -0,0 +1,61 @@
+/**
+ * See README for licensing information
+*/
+package net.part1kl.homekit.core.util;
+
+import java.util.ArrayList;
+
+/** TODO Put type description here
+ * 
+ * 
+ * <br><br>
+ * <style>
+ *		table {
+ *  		border-collapse: collapse;
+ *		}
+ *		table, tr {
+ *			border: 1px solid black;
+ *		}
+ *	</style>
+ * <table>
+ * 		<tr>
+ * 			<th> Project: </th>
+ * 			<td> HomeKit-Core </td>
+ * 		</tr>
+ * 		<tr>
+ * 			<th> Package: </th>
+ * 			<td> net.part1kl.homekit.core.util </td>
+ * 		</tr>
+ * 		<tr>
+ * 			<th> Class: </th>
+ * 			<td> KitArrayList </td>
+ * 		</tr>
+ * 		<tr>
+ * 			<th> Version: </th>
+ * 			<td> 0.1 </td>
+ * 		</tr>
+ * 		<tr>
+ * 			<th> Date Created: </th>
+ * 			<td> Aug 21, 2018 </td>
+ * 		</tr>
+ * 		<tr>
+ * 			<th> Author: </th>
+ * 			<td> part1kl </td>
+ * 		</tr>
+ * 		<tr>
+ * 			<th> Year: </th>
+ * 			<td> 2018 </td>
+ * 		</tr>
+ * </table>
+ */
+public class KitArrayList<T> extends ArrayList<T> {
+
+	public KitArrayList() { super(); }
+	
+	public KitArrayList(T... parameters) {
+		super();
+		for(T p : parameters) {
+			add(p);
+		}
+	}
+}
