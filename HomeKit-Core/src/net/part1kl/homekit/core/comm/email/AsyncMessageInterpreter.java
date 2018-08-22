0085a55699063eaf3538b61687af32534b9610f7
part1kl <part1kl@part1kl.net>
2018-08-22 00:19:44 -0400

    I haven't done much here yet, but this class will be tied into the Job system as a Looped Job, checking for incoming emails, interpreting them, and passing on the commands to the core runtime system.

---
+++ HomeKit-Core/src/net/part1kl/homekit/core/comm/email/AsyncMessageInterpreter.java
@@ -0,0 +1,66 @@
+/**
+ * See README for licensing information
+*/
+package net.part1kl.homekit.core.comm.email;
+
+import java.util.Date;
+import java.util.HashMap;
+
+import javax.mail.Message;
+
+/** TODO Put type description here
+ * 
+ * 
+ * <br><br>
+ * <style>
+ *		table {
+ *  		border-collapse: collapse;
+ *		}
+ *		table, tr {
+ *			border: 1px solid black;
+ *		}
+ *	</style>
+ * <table>
+ * 		<tr>
+ * 			<th> Project: </th>
+ * 			<td> HomeKit-Core </td>
+ * 		</tr>
+ * 		<tr>
+ * 			<th> Package: </th>
+ * 			<td> net.part1kl.homekit.core.comm.email </td>
+ * 		</tr>
+ * 		<tr>
+ * 			<th> Class: </th>
+ * 			<td> AsyncMessageInterpreter </td>
+ * 		</tr>
+ * 		<tr>
+ * 			<th> Version: </th>
+ * 			<td> 0.1 </td>
+ * 		</tr>
+ * 		<tr>
+ * 			<th> Date Created: </th>
+ * 			<td> Aug 20, 2018 </td>
+ * 		</tr>
+ * 		<tr>
+ * 			<th> Author: </th>
+ * 			<td> part1kl </td>
+ * 		</tr>
+ * 		<tr>
+ * 			<th> Year: </th>
+ * 			<td> 2018 </td>
+ * 		</tr>
+ * </table>
+ */
+public class AsyncMessageInterpreter implements Runnable {
+	
+	
+	
+	
+	public AsyncMessageInterpreter(Email async, HashMap<Date, Message> messages) {
+		
+	}
+	
+	public void run() {
+		
+	}
+}
